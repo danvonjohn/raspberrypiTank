10dea7c0b0545a73b18765ce2ed485f5033bf987
Dan Thorne <d_thorne@live.co.uk>
2020-12-08 22:45:00 +0000

    Update tank.py
    LED Mapping to pins added

--- tank.py
+++ tank.py
@@ -1,6 +1,9 @@
 #packages
 
 #Connection LED - Internet
+#LED - Green
+#Pin - 16
+#GPIO - 23
 ################################
 
 #Connection LED - Server
@@ -13,22 +16,40 @@
 ################################
 
 #Battery LED High
+#LED - Yellow
+#Pin - 15
+#GPIO - 22
 ################################
 
 #Battery LED Med
+#LED - Yellow
+#Pin - 15
+#GPIO - 22
 ################################
 
 #Battery LED Low
+#LED - Yellow
+#Pin - 15
+#GPIO - 22
 ################################
 
 #Battery LED Low Flashing
+#LED - Yellow
+#Pin - 15
+#GPIO - 22
 ################################
 
 #Forward
 ################################
 
 #Turn left
+#LED - Red
+#Pin - 12
+#GPIO - 18
 ################################
 
-#Turn right
+#Turn Right
+#LED - Blue
+#Pin - 11
+#GPIO - 17
 ################################
